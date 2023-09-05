cbcbd41bcc39e9252844c2ae3bfb759b8b24a780
Abhiraj Bhattashali <abhirajbhattashali@gmail.com>
2023-09-05 18:32:15 +0530

    Initial Commit

---
+++ ScreenshotLabelRecognizerr/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ScreenshotLabelRecognizerr
+//
+//  Created by Abhiraj on 05/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScreenshotLabelRecognizerr/ScreenshotLabelRecognizerrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotLabelRecognizerrApp.swift
+//  ScreenshotLabelRecognizerr
+//
+//  Created by Abhiraj on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotLabelRecognizerrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
